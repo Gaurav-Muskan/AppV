78ff11f385543b9cf69ea15747a8476326cd8475
Gaurav Muskan <gaurav.muskan@incture.com>
2024-03-14 08:47:11 +0000

    Greeting Message

--- project1/webapp/controller/First.controller.js
+++ project1/webapp/controller/First.controller.js
@@ -1,19 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller,MessageToast) {
-        "use strict";
+    "sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
+	"use strict";
+    // /**
+    //  * @param {typeof sap.ui.core.mvc.Controller} Controller
+    //  */
+    // function (Controller,MessageToast) {
+    //     "use strict";
 
         return Controller.extend("restinpeace.project1.controller.First", {
-            onInit: function() {
-                // Assuming your label has an ID "myLabel"
-                var oLabel = this.getView().byId("myLabel");
-                oLabel.addStyleClass("myCustomClass");
-            }  ,          
+            onInit() {
+                // set data model on view
+                const oData = {
+                   recipient : {
+                      name : "World"
+                   }
+                };
+                const oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+             },    
             onShowHello() {
                 MessageToast.show("Hello World");
               },
