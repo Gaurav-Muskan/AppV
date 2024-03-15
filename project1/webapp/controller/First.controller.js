182ba02567e2318c1f5abce0ff6de87662fc05c7
Gaurav Muskan <gaurav.muskan@incture.com>
2024-03-15 06:03:15 +0000

    ButtonOnFooter

--- project1/webapp/controller/First.controller.js
+++ project1/webapp/controller/First.controller.js
@@ -1,31 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
 ], (Controller, MessageToast, JSONModel) => {
-	"use strict";
-    // /**
-    //  * @param {typeof sap.ui.core.mvc.Controller} Controller
-    //  */
-    // function (Controller,MessageToast) {
-    //     "use strict";
+    "use strict";
 
-        return Controller.extend("restinpeace.project1.controller.First", {
-            onInit() {
-                // set data model on view
-                const oData = {
-                   recipient : {
-                      name : "World"
-                   }
-                };
-                const oModel = new JSONModel(oData);
-                this.getView().setModel(oModel);
-             },    
-            onShowHello() {
-                MessageToast.show("Hello World");
-              },
-            onGoSecond: function(){
-                this.getOwnerComponent().getRouter().navTo("Second");
-            }
-        });
+    return Controller.extend("restinpeace.project1.controller.First", {
+        onInit: function() {
+            // Initialization code here
+        },
+
+        onGoSecond: function() {
+            this.getOwnerComponent().getRouter().navTo("Second");
+        }
     });
+});
