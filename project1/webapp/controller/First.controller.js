9749aefcaa68b0d9d1a03793d70afcf52ae9b9c4
Gaurav Muskan <gaurav.muskan@incture.com>
2024-03-17 14:21:55 +0000

    WlcmAccToInputName

--- project1/webapp/controller/First.controller.js
+++ project1/webapp/controller/First.controller.js
@@ -7,7 +7,11 @@ sap.ui.define([
         onInit: function() {
             // Initialization code here
         },
-
+        OnSubmit: function() {
+            var name=this.getView().byId("idIpName").getValue();//We got the input which we have filled in the inbox.
+            var msg="Welcome to UI5 "+name;
+            this.getView().byId("idTxtMsg").setText(msg);
+        },
         onGoSecond: function() {
             this.getOwnerComponent().getRouter().navTo("Second");
         }
