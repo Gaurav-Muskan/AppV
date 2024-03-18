a85e6034c8569d1f1a7c26aa10639a67328f0aa5
Gaurav Muskan <gaurav.muskan@incture.com>
2024-03-18 06:05:21 +0000

    TextAlignChoiceByUser

--- project1/webapp/controller/First.controller.js
+++ project1/webapp/controller/First.controller.js
@@ -10,6 +10,9 @@ sap.ui.define([
         OnSubmit: function() {
             var name=this.getView().byId("idIpName").getValue();//We got the input which we have filled in the inbox.
             var msg="Welcome to UI5 "+name;
+            //For second task i.e show align acc. to user's choice.
+            var align=this.getView().byId("idIpAlign").getValue();
+            this.getView().byId("idTxtMsg").setTextAlign(align);
             this.getView().byId("idTxtMsg").setText(msg);
         },
         onGoSecond: function() {
