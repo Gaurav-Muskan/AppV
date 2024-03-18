36e70030560f26f13790129ee3ca863ffc8f0c0a
Gaurav Muskan <gaurav.muskan@incture.com>
2024-03-18 07:28:30 +0000

    Hide-ShowConfig

--- project1/webapp/controller/First.controller.js
+++ project1/webapp/controller/First.controller.js
@@ -1,25 +1,48 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-], (Controller, MessageToast, JSONModel) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, MessageToast, JSONModel) {
     "use strict";
 
     return Controller.extend("restinpeace.project1.controller.First", {
         onInit: function() {
             // Initialization code here
         },
-        OnSubmit: function() {
-            var name=this.getView().byId("idIpName").getValue();//We got the input which we have filled in the inbox.
-            var msg="Welcome to UI5 "+name;
-            //For second task i.e show align acc. to user's choice.
-            var align=this.getView().byId("idIpAlign").getValue();
+
+        onGoSecond: function() {
+            this.getOwnerComponent().getRouter().navTo("Second");
+        },
+
+        onSubmit: function() {
+            var name = this.getView().byId("idIpName").getValue();
+            var msg = "Welcome to UI5 " + name;
+            var align = this.getView().byId("idIpAlign").getValue();
             this.getView().byId("idTxtMsg").setTextAlign(align);
             this.getView().byId("idTxtMsg").setText(msg);
-            //disable input field...
             this.getView().byId("idIpName").setEnabled(false);
             this.getView().byId("idIpAlign").setEnabled(false);
         },
-        onGoSecond: function() {
-            this.getOwnerComponent().getRouter().navTo("Second");
+
+        onHideShow: function() {
+            var txtOnBtn = this.getView().byId("idBtnHideShow").getText();
+            if (txtOnBtn === "Hide") {
+                this.getView().byId("idTxtMsg").setVisible(false);
+                this.getView().byId("idLblName").setVisible(false);
+                this.getView().byId("idIpName").setVisible(false);
+                this.getView().byId("idLblAlign").setVisible(false);
+                this.getView().byId("idIpAlign").setVisible(false);
+
+                this.getView().byId("idBtnHideShow").setText("Show");
+            } else {
+                this.getView().byId("idTxtMsg").setVisible(true);
+                this.getView().byId("idLblName").setVisible(true);
+                this.getView().byId("idIpName").setVisible(true);
+                this.getView().byId("idLblAlign").setVisible(true);
+                this.getView().byId("idIpAlign").setVisible(true);
+
+                this.getView().byId("idBtnHideShow").setText("Hide");
+            }
         }
     });
 });
