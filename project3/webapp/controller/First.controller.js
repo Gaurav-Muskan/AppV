3e1abc12449af7e60ffd5feb47614604cbded5b6
Gaurav Muskan <gaurav.muskan@incture.com>
2024-06-04 16:21:52 +0000

    oData Connection Setup

--- project3/webapp/controller/First.controller.js
+++ project3/webapp/controller/First.controller.js
@@ -1,12 +1,48 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("incture.project3.controller.First", {
         onInit: function () {
+            // Initialize JSON Model
+            var mLookupModel = new JSONModel();
+            this.getView().setModel(mLookupModel, "mLookupModel");
+            this.mLookupModel = mLookupModel;
 
+            // Get Northwind Data Model
+            var NorthwindDataModel = this.getOwnerComponent().getModel("NorthwindDataModel");
+            this.NorthwindDataModel = NorthwindDataModel;
+
+            // Call function to fetch Employees data
+            this.getEmployeesSet();
+        },
+
+        getEmployeesSet: function () {
+            var sUrl = "/Employees";
+            var mLookupModel = this.mLookupModel;
+            var NorthwindDataModel = this.NorthwindDataModel;
+
+            NorthwindDataModel.read(sUrl, {
+                success: function (oData) {
+                    var aEmployeesGroupSet = oData.results;
+                    console.log("Data fetched successfully:", aEmployeesGroupSet); // Log fetched data
+                    mLookupModel.setProperty("/aEmployeesGroupSet", aEmployeesGroupSet);
+                    mLookupModel.refresh();
+                },
+                error: function (oError) {
+                    console.error("Error fetching data:", oError); // Log error
+                    mLookupModel.setProperty("/aEmployeesGroupSet", []);
+                    mLookupModel.refresh();
+                }
+            });
+        },
+
+        onComboBoxChange: function (oEvent) {
+            // Event handler for ComboBox change
+            var selectedKey = oEvent.getSource().getSelectedKey();
+            console.log("Selected key:", selectedKey);
         }
     });
 });
---
+++ project4/webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("incture.com.project4.controller.View1", {
+        onInit: function () {
+
+        }
+    });
+});
